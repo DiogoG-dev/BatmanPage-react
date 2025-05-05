c49276ef1643f1c31118fe191cdb041385ed2bff
DiogoGabriel <diogogabriel.tech@gmail.com>
2025-05-05 17:35:26 -0300

    Adding bannerBatman and trailer of content

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css';
+
+export function Footer() {
+    return (
+        <footer>
+            <span>© 2024 Diogo Gabriel. Todos os direitos reservados.</span>
+        </footer>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,29 @@
+import './Home.css';
 import { Header } from "../components/header/header";
+import { Footer } from "../components/footer/Footer";
 
 export function Home() {
     return (
-        <Header />
+        <>
+            <Header />
+            <div className="banner"></div>
+            <div className="trailer-container">
+                <div className="content">
+                    <video controls class='trailer'>
+                        <source src='video/BATMAN_Trailer_3_Oficial_Dublado.mp4' type='video/mp4' />
+                    </video>
+                    <div className="sinopse">
+                        <p className='description'>
+                            Batman (The Batman, no original) segue o segundo ano de Bruce Wayne (Robert Pattinson) como o herói de Gotham, 
+                            enfrentando o crime e a corrupção na cidade. Com a ajuda de poucos aliados, ele investiga os crimes do vilão 
+                            Charada e revela uma trama envolvendo sua família. Para combater a corrupção e descobrir a verdade sobre seu pai, 
+                            Thomas Wayne, Batman precisa formar novas alianças e buscar justiça.
+                        </p>
+                        <button className='buy-button'>Comprar Ingresso</button>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </>
     )
 }
