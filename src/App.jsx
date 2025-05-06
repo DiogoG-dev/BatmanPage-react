d1de01e40b9da168e0dcde22936b354ae0f76786
DiogoGabriel <diogogabriel.tech@gmail.com>
2025-05-06 16:37:56 -0300

    Adding Comments Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home/Home';
 import { Photos } from './pages/Photos/Photos';
+import { Comments } from './pages/Comments/Comments'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/photos' element={<Photos />} />
+      <Route path='/comments' element={<Comments />} />
     </Routes>
   )
 }
---
+++ src/pages/Comments/Comments.jsx
@@ -0,0 +1,217 @@
+import './Comments.css'
+import { Footer } from '../../components/footer/Footer'
+import { Header } from '../../components/header/header'
+
+export function Comments() {
+    return (
+        <>
+            <Header />
+            <div className="comments-container">
+                <div className="comments-content">
+                    <h3>26.7K comentários</h3>
+                    <div className="div-comment">
+                        <div className="img img-1"></div>
+                        <div className="comment-info">
+                            <div className="name-comment">
+                                <div className="name">João Pimenta</div>
+                                <div className="comment">
+                                    Simplesmente fantástico! A atmosfera sombria e o mistério foram perfeitos. O Robert Pattinson trouxe uma nova 
+                                    visão para o Batman, mais introspectivo e cheio de falhas. #BatMan
+                                </div>
+                            </div>
+                            <div className="date-likes">
+                                <div className="date">04/04/2022</div>
+                                <div className="likes">❤️ 47</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="div-comment">
+                        <div className="img img-2"></div>
+                        <div className="comment-info">
+                            <div className="name-comment">
+                                <div className="name">Mário Padrão de Beleza</div>
+                                <div className="comment">
+                                    O Riddler, interpretado por Paul Dano, foi uma surpresa. Sua interpretação sombria e perturbadora trouxe um 
+                                    novo nível de tensão ao filme, mas em certos momentos, seu vilão se perdeu no excesso de simbolismos, o que 
+                                    pode acabar afastando alguns espectadores que não estão familiarizados com a profundidade do personagem. A 
+                                    crítica social do filme, embora bem-intencionada, às vezes parece forçada e pouco sutil.
+                                </div>
+                            </div>
+                            <div className="date-likes">
+                                <div className="date">12/05/2022</div>
+                                <div className="likes">❤️ 83</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="div-comment">
+                        <div className="img img-3"></div>
+                        <div className="comment-info">
+                            <div className="name-comment">
+                                <div className="name">Maria José</div>
+                                <div className="comment">
+                                    O Riddler foi genial! Um vilão que dá medo de verdade. E o filme tem uma vibe de investigação que me fez pensar 
+                                    que estava assistindo a um thriller policial, não um filme de super-herói!" 
+                                </div>
+                            </div>
+                            <div className="date-likes">
+                                <div className="date">23/06/2022</div>
+                                <div className="likes">❤️ 12</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="div-comment">
+                        <div className="img img-4"></div>
+                        <div className="comment-info">
+                            <div className="name-comment">
+                                <div className="name">Zé da Roça</div>
+                                <div className="comment">
+                                    Robert Pattinson como Batman? Achei que fosse o Bruce Wayne version 'emo' – mas até que ele ficou bem nesse 
+                                    papel de 'você não entende minha dor'. 😂😂😂
+                                </div>
+                            </div>
+                            <div className="date-likes">
+                                <div className="date">15/07/2022</div>
+                                <div className="likes">❤️ 56</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="div-comment">
+                        <div className="img img-5"></div>
+                        <div className="comment-info">
+                            <div className="name-comment">
+                                <div className="name">Gabriel Santos</div>
+                                <div className="comment">
+                                    Batman 2022 é basicamente um filme de 3 horas de Robert Pattinson olhando para o vazio e refletindo sobre a 
+                                    vida. Ele realmente se esforçou para ser o Batman mais 'emo' de todos os tempos. Eu esperava mais ação, mas no 
+                                    fim das contas, o filme parece mais um drama existencial sobre a solidão de um bilionário com uma capa. 🙄
+                                </div>
+                            </div>
+                            <div className="date-likes">
+                                <div className="date">30/08/2022</div>
+                                <div className="likes">❤️ 29</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="div-comment">
+                        <div className="img img-6"></div>
+                        <div className="comment-info">
+                            <div className="name-comment">
+                                <div className="name">Raissa Stéfany</div>
+                                <div className="comment">
+                                    A cinematografia é, sem dúvida, um dos pontos altos do filme. A maneira como a cidade de Gotham é filmada, com 
+                                    uma estética sombria e um jogo de luzes e sombras, realmente consegue transmitir a sensação de opressão e 
+                                    decadência. No entanto, o som e a trilha sonora de Michael Giacchino são igualmente impressionantes, com uma 
+                                    abordagem minimalista que complementa a atmosfera de mistério e tensão do filme.
+                                </div>
+                            </div>
+                            <div className="date-likes">
+                                <div className="date">22/09/2022</div>
+                                <div className="likes">❤️ 71</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="div-comment">
+                        <div className="img img-7"></div>
+                        <div className="comment-info">
+                            <div className="name-comment">
+                                <div className="name">Léo Ladrão de Coração</div>
+                                <div className="comment">
+                                    Assistindo Batman 2022 e pensando... será que ele está investigando crimes ou só tentando achar uma saída para 
+                                    o conflito existencial de ser rico e triste? 🤔🤔
+                                </div>
+                            </div>
+                            <div className="date-likes">
+                                <div className="date">05/11/2022</div>
+                                <div className="likes">❤️ 64</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="div-comment">
+                        <div className="img img-8"></div>
+                        <div className="comment-info">
+                            <div className="name-comment">
+                                <div className="name">Tereza Bagunça</div>
+                                <div className="comment">
+                                    Amei esse filme o Batman! A química entre o Bruce e a Selina foi incrível, e o vilão Riddler me deixou com medo 
+                                    real. Melhor do que eu esperava! 😲
+                                </div>
+                            </div>
+                            <div className="date-likes">
+                                <div className="date">18/12/2022</div>
+                                <div className="likes">❤️ 17</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="div-comment">
+                        <div className="img img-9"></div>
+                        <div className="comment-info">
+                            <div className="name-comment">
+                                <div className="name">Felipe Rocha</div>
+                                <div className="comment">
+                                    O Batman 2022 traz uma abordagem mais sombria e introspectiva do herói, focando no detetive mais do que no 
+                                    vigilante. O estilo noir do filme é claramente evidente, com uma Gotham sombria e quase opressiva, que serve 
+                                    como um personagem por si só. A atuação de Robert Pattinson foi sólida, mas não consigo deixar de sentir que o 
+                                    filme poderia ter explorado mais a dualidade entre Bruce Wayne e o Batman.
+                                </div>
+                            </div>
+                            <div className="date-likes">
+                                <div className="date">09/02/2023</div>
+                                <div className="likes">❤️ 77</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="div-comment">
+                        <div className="img img-10"></div>
+                        <div className="comment-info">
+                            <div className="name-comment">
+                                <div className="name">Margarida Vida Loka</div>
+                                <div className="comment">
+                                    Gotham é tão sombria e triste que até a chuva parece deprimida. O Batman poderia passar um tempo na terapia 
+                                    para lidar com tanta angústia. Ele é o único super-herói que parece ter esquecido a definição de 'esperança'. 😫
+                                </div>
+                            </div>
+                            <div className="date-likes">
+                                <div className="date">09/02/2023</div>
+                                <div className="likes">❤️ 39</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="div-comment">
+                        <div className="img img-11"></div>
+                        <div className="comment-info">
+                            <div className="name-comment">
+                                <div className="name">Tião Cabeludo</div>
+                                <div className="comment">
+                                    Eu assisti Batman 2022 e pensei: 'Se ele passar mais 3 horas resmungando na Batcaverna, vai precisar de um chá 
+                                    e uma sessão de terapia'. Ele que me chame, sou psicólogo de super-heróis!" 🤣🤣
+                                </div>
+                            </div>
+                            <div className="date-likes">
+                                <div>18/12/2024</div>
+                                <div className="likes">❤️ 57</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="div-comment">
+                        <div className="img img-12"></div>
+                        <div className="comment-info">
+                            <div className="name-comment">
+                                <div className="name">Sofia Almeida</div>
+                                <div className="comment">
+                                    Não sou muito fã de Batman, mas esse filme me pegou de surpresa! A tensão, o suspense, o trabalho de câmera… 
+                                    sem falar que a química entre o Batman e a Catwoman foi um espetáculo à parte! 💘
+                                </div>
+                            </div>
+                            <div className="date-likes">
+                                <div>23/12/2024</div>
+                                <div className="likes">❤️ 9</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="see-more">Ver Mais ⬇</div>
+                </div>
+            </div>
+            <Footer />
+        </>
+    )
+}
